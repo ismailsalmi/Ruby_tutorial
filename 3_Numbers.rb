bc92c33628e4a0705e7e95d02c98105ae4f8d626
Ismail Salmi <ismail.man.2013@gmail.com>
2020-04-30 00:05:22 +0000

    Update 3_Numbers.rb

--- 3_Numbers.rb
+++ 3_Numbers.rb
@@ -1,41 +1,41 @@
 numbers = 10
-puts numbers          # Output 10
-puts numbers + 10     # Output 20
-puts numbers - 10     # Output 0
-puts numbers / 2      # Output 5
-puts numbers * 2      # Output 20
-puts numbers < 10     # Output false
-puts numbers < 11     # Output true
-puts numbers <= 10    # Output true
-puts numbers > 20     # Output false
-puts numbers > 9      # Output true
-puts numbers >= 8     # Output true
-puts numbers % 4      # Output 2
-puts numbers % 3 == 1 # Output true
-puts numbers % 5 == 0 # Output true
-puts numbers + 5 * 4  # Output 30
-puts numbers + 5 / 4  # Output 11
-puts numbers == 10    # Output true
-puts numbers == 10.0  # Output true
-puts numbers == 10.1  # Output false
-puts numbers.abs      # Output 10
-puts numbers.bit_length   # Output 4
-puts (numbers + 55).chr   # Output A
-puts numbers.digits       # Output [0, 1]
-puts numbers.digits 7     # Output [3, 1]
-puts numbers.floor        # Output 10
-puts -numbers.floor(-2)   # Output 0
-puts numbers.gcd(2)       # Output 2
-puts numbers.gcdlcm(2)    # Output [2, 10]
-puts numbers.gcdlcm(-7)   # Output [1, 70]
-puts numbers.next         # Output 11
-puts (-numbers).next      # Output -9
-puts numbers.succ         # Output 11
-puts (10-numbers).succ    # Output 1
-puts numbers.ord          # Output 10
-puts numbers.pred         # Output 9
-puts (-numbers).pred      # Output -11
-puts numbers.remainder(3) # Output 1
+puts numbers                  # Output 10
+puts numbers + 10             # Output 20
+puts numbers - 10             # Output 0
+puts numbers / 2              # Output 5
+puts numbers * 2              # Output 20
+puts numbers < 10             # Output false
+puts numbers < 11             # Output true
+puts numbers <= 10            # Output true
+puts numbers > 20             # Output false
+puts numbers > 9              # Output true
+puts numbers >= 8             # Output true
+puts numbers % 4              # Output 2
+puts numbers % 3 == 1         # Output true
+puts numbers % 5 == 0         # Output true
+puts numbers + 5 * 4          # Output 30
+puts numbers + 5 / 4          # Output 11
+puts numbers == 10            # Output true
+puts numbers == 10.0          # Output true
+puts numbers == 10.1          # Output false
+puts numbers.abs              # Output 10
+puts numbers.bit_length       # Output 4
+puts (numbers + 55).chr       # Output A
+puts numbers.digits           # Output [0, 1]
+puts numbers.digits 7         # Output [3, 1]
+puts numbers.floor            # Output 10
+puts -numbers.floor(-2)       # Output 0
+puts numbers.gcd(2)           # Output 2
+puts numbers.gcdlcm(2)        # Output [2, 10]
+puts numbers.gcdlcm(-7)       # Output [1, 70]
+puts numbers.next             # Output 11
+puts (-numbers).next          # Output -9
+puts numbers.succ             # Output 11
+puts (10-numbers).succ        # Output 1
+puts numbers.ord              # Output 10
+puts numbers.pred             # Output 9
+puts (-numbers).pred          # Output -11
+puts numbers.remainder(3)     # Output 1
 puts (5-numbers).remainder(3) # Output -2
 puts numbers.round            # Output 10
 puts numbers.round(-3)        # Output 0
